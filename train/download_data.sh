b8fe729e0a55081dde4ab3963492b4a95f06c473
koo <jakeoung@users.noreply.github.com>
2022-08-11 05:38:06 +0800

    Update download_data.sh

--- train/download_data.sh
+++ train/download_data.sh
@@ -1,12 +1,12 @@
 # echo "Download middlebury dataset and extract to ../data/middlebury/raw"
-# wget https://vision.middlebury.edu/stereo/data/scenes2005/ThirdSize/zip-2views/ALL-2views.zip
+# wget https://vision.middlebury.edu/stereo/data/scenes2005/HalfSize/zip-2views/ALL-2views.zip
 # mkdir ../data
 # mkdir ../data/middlebury
 # mkdir ../data/middlebury/raw
 # unzip ALL-2views.zip -d ../data/middlebury/raw
 # rm ALL-2views.zip
 
-# wget https://vision.middlebury.edu/stereo/data/scenes2006/ThirdSize/zip-2views/ALL-2views.zip
+# wget https://vision.middlebury.edu/stereo/data/scenes2006/HalfSize/zip-2views/ALL-2views.zip
 # unzip ALL-2views.zip -d ../data/middlebury/raw
 # rm ALL-2views.zip
 
