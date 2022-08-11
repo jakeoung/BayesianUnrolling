39a6477671bee9ddcacdede65233e98996b3ecd0
koo <jakeoung@users.noreply.github.com>
2022-08-11 20:25:18 +0800

    Update download_data.sh

--- train/download_data.sh
+++ train/download_data.sh
@@ -1,18 +1,16 @@
-# echo "Download middlebury dataset and extract to ../data/middlebury/raw"
-# wget https://vision.middlebury.edu/stereo/data/scenes2005/HalfSize/zip-2views/ALL-2views.zip
-# mkdir ../data
-# mkdir ../data/middlebury
-# mkdir ../data/middlebury/raw
-# unzip ALL-2views.zip -d ../data/middlebury/raw
-# rm ALL-2views.zip
+echo "Download middlebury dataset and extract to ../data/middlebury/raw"
+wget https://vision.middlebury.edu/stereo/data/scenes2005/HalfSize/zip-2views/ALL-2views.zip
+mkdir -p ../data/middlebury/raw
+unzip ALL-2views.zip -d ../data/middlebury/raw
+rm ALL-2views.zip
 
-# wget https://vision.middlebury.edu/stereo/data/scenes2006/HalfSize/zip-2views/ALL-2views.zip
-# unzip ALL-2views.zip -d ../data/middlebury/raw
-# rm ALL-2views.zip
+wget https://vision.middlebury.edu/stereo/data/scenes2006/HalfSize/zip-2views/ALL-2views.zip
+unzip ALL-2views.zip -d ../data/middlebury/raw
+rm ALL-2views.zip
 
-# cd ../data
-# mkdir mpi
-# cd mpi
-# wget http://files.is.tue.mpg.de/jwulff/sintel/MPI-Sintel-stereo-training-20150305.zip
-# unzip MPI-Sintel-stereo-training-20150305.zip
-# rm MPI-Sintel-stereo-training-20150305.zip
+cd ../data
+mkdir mpi
+cd mpi
+wget http://files.is.tue.mpg.de/jwulff/sintel/MPI-Sintel-stereo-training-20150305.zip
+unzip MPI-Sintel-stereo-training-20150305.zip
+rm MPI-Sintel-stereo-training-20150305.zip
